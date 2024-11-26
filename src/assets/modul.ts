72197765d6119b72325bd5be2ab53944eb55c12e
Tobias Molinero <tobiasmolinero98@gmail.com>
2024-11-25 22:01:13 -0300

    seccion skills

---
+++ src/assets/modul.ts
@@ -0,0 +1,29 @@
+import astroDark from './icons/Astro_dark.svg'
+import astroLight from './icons/Astro_light.svg'
+import bootstrap from './icons/bootstrap.svg'
+import tailwind from './icons/tailwindcss.svg'
+import expressDark from './icons/Express.js_dark.svg'
+import expressLight from './icons/Express.js_light.svg'
+import git from './icons/git.svg'
+import javascript from './icons/javascript.svg'
+import typescript from './icons/typescript.svg'
+import node from './icons/nodejs.svg'
+import react from './icons/react.svg'
+import svelte from './icons/svelte.svg'
+import mysql from './icons/mysql.svg'
+
+export {
+    astroDark, 
+    astroLight,
+    bootstrap,
+    tailwind,
+    expressDark,
+    expressLight,
+    git,
+    javascript,
+    typescript,
+    node,
+    react,
+    svelte,
+    mysql,
+}
---
+++ src/utils/skils.ts
@@ -0,0 +1,27 @@
+import {
+    astroDark, 
+    bootstrap, 
+    git, 
+    tailwind,
+    react, 
+    svelte, 
+    mysql, 
+    expressDark, 
+    node, 
+    javascript, 
+    typescript
+} from '../assets/modul'
+
+export const skills = [
+    {title: "React", src: react.src, alt: "Icono de react"},
+    {title: "Svelte", src: svelte.src, alt: "Icono de svelte"},
+    {title: "Astro", src: astroDark.src, alt: "Icono de astro"},
+    {title: "Bootstrap", src: bootstrap.src, alt: "Icono de bootstrap"},
+    {title: "Tailwind", src: tailwind.src, alt: "Icono de tailwind"},
+    {title: "Javascript", src: javascript.src, alt: "Icono de Javascript"},
+    {title: "Typescript", src: typescript.src, alt: "Icono de typescript"},
+    {title: "Node js", src: node.src, alt: "Icono de node"},
+    {title: "Express js", src: expressDark.src, alt: "Icono de express"},
+    {title: "MySQL", src: mysql.src, alt: "Icono de mysql"},
+    {title: "Git", src: git.src, alt: "Icono de git"},
+]
